c4548aac1c61dd3b04e4aa461b36a17eef4e59ba
haoyuh3 <haoyuh3>
2025-05-04 15:54:35 +0800

    "编辑员工功能"

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -10,36 +10,36 @@ public interface EmployeeService {
 
     /**
      * 员工登录
-     * @param employeeLoginDTO
-     * @return
+     * @param employeeLoginDTO  Data transfer object containing the employee's login credentials
+     * @return Employee object containing employee details.
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
     void save(EmployeeDTO employeeDTO);
 
     /**
      * 分页查询员工
-     * @param employeePageQueryDTO
-     * @return
+     * @param employeePageQueryDTO Data transfer object containing the employee's login credentials (e.g., username and password).
+     * @return PageResult containing the paginated list of employees.
      */
     PageResult page(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
      * 修改员工状态
-     * @param status
-     * @param id
+     * @param status Employee status (e.g., active, inactive).
+     * @param id employee id
      */
     void startOrStop(Integer status, Long id);
 
     /**
      * 根据id查询员工信息
-     * @param id
-     * @return
+     * @param id employee id
+     * @return Employee object containing employee details.
      */
     Employee getById(Long id);
 
     /**
      * 修改员工信息
-     * @param employeeDTO
+     * @param employeeDTO Data transfer object containing the employee's details (e.g., name, phone, etc.).
      */
     void update(EmployeeDTO employeeDTO);
 }
