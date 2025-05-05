b60d698812dd18e5b5cfef8d47635112cd6d1760
haoyuh3 <haoyuh3>
2025-05-05 20:14:06 +0800

    "新增菜品功能"

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,8 @@ package com.sky.service;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
     /**
@@ -9,4 +11,11 @@ public interface DishService {
      * @param dishDTO 菜品DTO
      */
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 分页查询菜品
+     * @param dishPageQueryDTO 菜品分页查询DTO
+     * @return PageResult
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.entity.Employee;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +53,22 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.insertBatch(flavors);
 
         }
+    }
+
+    /**
+     * 分页查询菜品
+     * @param dishPageQueryDTO 菜品分页查询DTO
+     * @return PageResult
+     */
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        //分页查询
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+
+        long total = page.getTotal();
+        List<DishVO> list = page.getResult();
+
+        return new PageResult(total, list);
+
     }
 }
